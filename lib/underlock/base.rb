2512f5a08926b5ad1bffa98dc578b2fc58cf6eca
Jasdeep Singh <narang.jasdeep@gmail.com>
2017-02-02 20:12:28 -0500

    encrypt valid string file paths as strings only: fixes issue #3

--- lib/underlock/base.rb
+++ lib/underlock/base.rb
@@ -10,12 +10,9 @@ module Underlock
     class << self
 
       def encrypt(unencrypted_value)
-        if Pathname.new(unencrypted_value).exist?
-          unencrypted_value = File.new(unencrypted_value)
-        end
         case unencrypted_value
-          when File   then FileEncryptor.new.encrypt(unencrypted_value)
-          when String then Encryptor.new.encrypt(unencrypted_value)
+          when File, Pathname   then FileEncryptor.new.encrypt(unencrypted_value)
+          when String           then Encryptor.new.encrypt(unencrypted_value)
         end
       end
 
--- spec/underlock/base_spec.rb
+++ spec/underlock/base_spec.rb
@@ -52,40 +52,56 @@ describe Underlock::Base do
 
       end
     end
+
+    context 'when passing a string that is a valid path' do
+
+      let(:message) { './spec/files/file.txt' }
+
+      it 'should be able to decrypt the gibberish back to the actual path' do
+        expect(Underlock::Base.decrypt(secret)).to eq(message)
+      end
+
+    end
   end
 
   context 'file encryption' do
     let(:secret)  { Underlock::Base.encrypt(file) }
 
     %w(file.pdf file.txt).each do |filename|
-      let(:file)    { File.open("./spec/files/#{filename}") }
-
-      context 'encrypt' do
-        
-        it 'should return an instance of EncryptedEntity' do
-          expect(secret).to be_an_instance_of(Underlock::EncryptedEntity)
+      %w(file pathname).each do |elem|
+        if elem == 'file'
+          let(:file)    { File.open("./spec/files/#{filename}") }
+        else
+          let(:file)    { Pathname.new("./spec/files/#{filename}") }
         end
 
-        it 'should respond to #encrypted_file' do
-          expect(secret).to respond_to(:encrypted_file)
+        context 'encrypt' do
+          
+          it 'should return an instance of EncryptedEntity' do
+            expect(secret).to be_an_instance_of(Underlock::EncryptedEntity)
+          end
+
+          it 'should respond to #encrypted_file' do
+            expect(secret).to respond_to(:encrypted_file)
+          end
+
+          it 'should not have the original value' do
+            expect(File.read(secret.encrypted_file)).to_not eq(File.read(file))
+          end
         end
 
-        it 'should not have the original value' do
-          expect(File.read(secret.encrypted_file)).to_not eq(File.read(file))
-        end
-      end
+        context 'decrypt' do
+          let(:encrypted_entity) { Underlock::Base.encrypt(file) }
 
-      context 'decrypt' do
-        let(:encrypted_entity) { Underlock::Base.encrypt(file) }
+          it 'should be able to return a file for an encrypted file' do
+            expect(Underlock::Base.decrypt(encrypted_entity)).to be_an_instance_of(File)
+          end
 
-        it 'should be able to return a file for an encrypted file' do
-          expect(Underlock::Base.decrypt(encrypted_entity)).to be_an_instance_of(File)
-        end
-
-        it 'should be able to read the contents of the file' do
-          decrypted_file = Underlock::Base.decrypt(encrypted_entity)
-          yomu = Yomu.new(decrypted_file.to_path)
-          expect(yomu.text.strip).to eq("super secret message in the pdf file")
+          it 'should be able to read the contents of the file' do
+            decrypted_file = Underlock::Base.decrypt(encrypted_entity)
+            yomu = Yomu.new(decrypted_file.to_path)
+            expect(yomu.text.strip).to eq("super secret message in the pdf file")
+          end
         end
       end
     end
